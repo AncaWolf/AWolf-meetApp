79b53e4684b14bcbeaae5bc88df3abd42abc8b26
Anca <anca.m.gozman@gmail.com>
2024-02-22 21:04:32 +0100

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,12 +1,20 @@
-// src/__tests__/App.test.js
-
 import { render } from '@testing-library/react';
-import App from '../App';
+import EventList from '../components/EventList';
 
-describe('<App /> component', () => {
-  test('renders list of events', () => {
-    const AppDOM = render(<App />).container.firstChild;
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+describe('<EventList /> component', () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  })
+
+  test('has an element with "list" role', () => {
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
+  test('renders correct number of events', () => {
+    EventListComponent.rerender(<EventList events={
+      [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+    } />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+  });
 });
