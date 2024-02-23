dcc2e976cb7b7a16084b071d74f7b009dd63d058
Anca <anca.m.gozman@gmail.com>
2024-02-23 21:32:34 +0100

    deleted eslint comments (after disabling eslint)

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -6,28 +6,23 @@ import CitySearch from '../components/CitySearch';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    // eslint-disable-next-line testing-library/no-render-in-setup
     CitySearchComponent = render(<CitySearch />);
   });
   test('renders text input', () => {
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     expect(cityTextBox).toBeInTheDocument();
     expect(cityTextBox).toHaveClass('city');
   });
 
   test('suggestions list is hidden by default', () => {
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const suggestionList = CitySearchComponent.queryByRole('list');
     expect(suggestionList).not.toBeInTheDocument();
   });
 
   test('renders a list of suggestions when city textbox gains focus', async () => {
     const user = userEvent.setup();
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     await user.click(cityTextBox);
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const suggestionList = CitySearchComponent.queryByRole('list');
     expect(suggestionList).toBeInTheDocument();
     expect(suggestionList).toHaveClass('suggestions');
@@ -40,7 +35,6 @@ describe('<CitySearch /> component', () => {
     CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
     // user types "Berlin" in city textbox
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     await user.type(cityTextBox, "Berlin");
 
@@ -50,7 +44,6 @@ describe('<CitySearch /> component', () => {
     }) : [];
 
     // get all <li> elements inside the suggestion list
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
     expect(suggestionListItems).toHaveLength(suggestions.length + 1);
     for (let i = 0; i < suggestions.length; i += 1) {
@@ -64,12 +57,10 @@ describe('<CitySearch /> component', () => {
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     await user.type(cityTextBox, "Berlin");
 
     // the suggestion's textContent look like this: "Berlin, Germany"
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
 
     await user.click(BerlinGermanySuggestion);
