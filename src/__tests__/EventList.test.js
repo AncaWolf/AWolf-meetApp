748ea298355cb39721480a731f954fd35ff8baf7
Anca <anca.m.gozman@gmail.com>
2024-02-23 19:34:21 +0100

    imported getEvents from api

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,9 +1,12 @@
 import { render } from '@testing-library/react';
+import { getEvents } from '../api';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
   test('has an element with "list" role', () => {
+    // eslint-disable-next-line testing-library/render-result-naming-convention
     const EventListComponent = render(<EventList />);
+    // eslint-disable-next-line testing-library/prefer-screen-queries, testing-library/prefer-presence-queries
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
