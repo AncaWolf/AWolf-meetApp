dd6b781ca121d6bd758d0d7140e89d144f6998a5
Anca <anca.m.gozman@gmail.com>
2024-02-23 20:56:14 +0100

    replaced simple objects test with test to retrieve mock data

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -3,17 +3,18 @@ import { getEvents } from '../api';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  })
+
   test('has an element with "list" role', () => {
-    // eslint-disable-next-line testing-library/render-result-naming-convention
-    const EventListComponent = render(<EventList />);
-    // eslint-disable-next-line testing-library/prefer-screen-queries, testing-library/prefer-presence-queries
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
-  test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={
-      [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-    } />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+  test('renders correct number of events', async () => {
+    const allEvents = await getEvents();
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
 });
