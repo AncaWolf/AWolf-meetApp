9a74e70aeb74797d011ec252dae037729bc5eb4d
Anca <anca.m.gozman@gmail.com>
2024-02-24 13:02:30 +0100

    added elements for event start time, location and title

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,21 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <li></li>
+    <li className="event">
+      <h4>{event.summary}</h4>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button className="details-button" onClick={() => setShowDetails(!showDetails)}>{showDetails ? "hide details" : "show details"}</button>
+      {showDetails ? (
+        <div className="details">
+          <h3>Event details</h3>
+          <p>{event.description}</p>
+        </div>
+      ) : null}
+    </li>
   );
 }
 
