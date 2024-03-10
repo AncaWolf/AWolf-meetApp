296b717f5bcd3db4c6f6736afd94ca2d25f6a0e9
Anca <anca.m.gozman@gmail.com>
2024-03-10 16:32:37 +0100

    Exercise 4.8 updates
    - Created Alert.js
    - added InfoAlert to App.js
    - added InfoAlert styling to App.css
    - added dummy prop function to CitySearch.js (to fix test error on InfoAlert)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
-
+import { InfoAlert } from './components/Alert';
 import './App.css';
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -28,7 +29,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <div className="alerts-container">{infoAlert.length ? <InfoAlert text={infoAlert} /> : null}</div>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} />
       <EventList events={events} />
     </div>
