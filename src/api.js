ed16fff4c0a5a0d0e523838e2193d2187b4a54e7
Anca <anca.m.gozman@gmail.com>
2024-03-12 20:11:21 +0100

    Task 4.9 - added offline capability (api.js update)

--- src/api.js
+++ src/api.js
@@ -32,6 +32,13 @@ export const getEvents = async () => {
         // NProgress.done();
         return mockData;
     }
+
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
     if (token) {
         removeQuery();
@@ -39,6 +46,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
