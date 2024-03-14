a828fa3e102a8dba75ccecb10086c5d75d706550
Anca <anca.m.gozman@gmail.com>
2024-03-14 21:37:55 +0100

    Task 4.10 - part 1
    - installed recharts (updated package.json and package-lock.json)
    - created CityEventChart.js and added relevant code
    - updated App.js to return chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import CityEventChart from './components/CityEventChart';
 import './App.css';
 
 const App = () => {
@@ -44,6 +45,7 @@ const App = () => {
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <CityEventChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
