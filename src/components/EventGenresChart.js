4ffe73283f5c7adb94fddbae9202b1b1f9385254
Anca <anca.m.gozman@gmail.com>
2024-03-18 21:09:47 +0100

    Task 4.10 - part 4
    - added useMemo() to 'genres' array in EventGenresChart.js to avoid endless re-rendering

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,26 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenresChart = ({ events }) => {
     const [data, setData] = useState([]);
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-    const colors = ['#DD0000', '#00DD00', '#0000DD', '#DDDD00', '#DD00DD']
+    const genres = useMemo(() => ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'], []); //wrapping `genres` in `useMemo` to avoid endless re-renders
+    const colors = ['#DD0000', '#00DD00', '#0000DD', '#DDDD00', '#DD00DD'];
+
 
     useEffect(() => {
+        console.log("Events received:", events); //added console.log
+        const getData = () => {
+            const data = genres.map((genre) => {
+                const filteredEvents = events.filter(event => event.summary.includes(genre)).length;
+                console.log("Filtered events for", genre, ":", filteredEvents);
+                return {
+                    name: genre,
+                    value: filteredEvents
+                };
+            });
+            return data;
+        };
         setData(getData());
-    }, [`${events}`]);
-    const getData = () => {
-        const data = genres.map((genre) => {
-            const filteredEvents = events.filter(event => event.summary.includes(genre)).length;
-            return {
-                name: genre,
-                value: filteredEvents.length
-            };
-        });
-        return data;
-    };
+    }, [genres, events]);
 
-    // }, [genres, events]);
 
     const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
         const RADIAN = Math.PI / 180;
@@ -52,10 +54,11 @@ const EventGenresChart = ({ events }) => {
                     outerRadius={150}
                 >
                     {
-                        data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={colors[index]} />
-                        ))
-                    }
+                        data.map((entry, index) => {
+                            return (
+                                <Cell key={`cell-${index}`} fill={colors[index]} />
+                            );
+                        })}
                 </Pie>
             </PieChart>
         </ResponsiveContainer>
